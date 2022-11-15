42763cf1969d7d8177544c6f2a4902811400ea9a
matt92101 <matt92101@hotmail.com>
2022-11-15 14:31:06 -0800

    Worked through US-2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
 
+
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null)) 
-  const [reset, setReset] = useState(false)
-  const [winner, setWinner] = useState('')
-  const resetBoard = () => (
-    setReset(true)
-  )
- 
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+
   const players = {
    Player1: {
     symbol: "⭕️",
@@ -23,23 +20,53 @@ const App = () => {
 
 
 
+  const renderSquare = (i) => {
+    return (
+        <Square value={squares[i]} onClick={() => handleClick(i)} />
+    )
+}
 
+const handleClick = (i) => {
+    const square = squares.slice();
+    symbol: "X",
+    square[i] = "",
+    setSquares(square)
 
-const handleGamePlay = (index) => {}
+}
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      
       <div className="gameboard">
+      <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+
         {squares.map((value, index) => {
           return (
             <Square 
             />
+            
           )
         })}
         </div>
     </>
-  )
+  
+
+            )
 }
 
 export default App
